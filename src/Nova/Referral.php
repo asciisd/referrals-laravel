9c112d7d4a3404480b7872e1446a7a331f8d8b5d
Mohamed Elogail <moh.elogail@gmail.com>
2023-09-20 13:43:04 +0300

    First commit

---
+++ src/Nova/Referral.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Referral extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Referral>
+     */
+    public static $model = \App\Models\User::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'first_name' . ' ' . 'last_name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+        'first_name',
+        'last_name',
+        'referral_token'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('First Name')->sortable(),
+            Text::make('Last Name')->sortable(),
+            Text::make('Email')->sortable(),
+            Text::make('Referral Token')->sortable(),
+            Number::make('# Referred Users', 'get_referrals_count')->sortable(),
+
+
+            HasMany::make('Referral', 'getReferrals')
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        return $query->where('referral_token', '<>', '')
+            ->withCount('getReferrals');
+    }
+}
---
+++ src/Traits/Referrerable.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Uuid;
+
+trait Referrerable
+{
+
+
+    /**
+     * Check if user is registered with referral token or not.
+     *
+     * @return bool
+     */
+    public function isReferred(): bool
+    {
+        return isset($this->referrer_id);
+    }
+
+    /**
+     * Self many-to-one relationship.
+     *
+     * @return HasMany
+     */
+    public function getReferrals(): HasMany
+    {
+        return $this->hasMany($this);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function getReferrer(): BelongsTo
+    {
+        return $this->belongsTo($this, 'referrer_id', 'id');
+    }
+
+    /**
+     * Check if user has referral token.
+     *
+     * @return bool
+     */
+    public function hasReferralToken(): bool
+    {
+        return isset($this->referral_token);
+    }
+
+    /**
+     * Generate referral token if not exist.
+     *
+     * @return bool
+     */
+    public function generateReferralToken(): bool
+    {
+        if (!$this->hasReferralToken()) {
+            return $this->update(['referral_token' => Str::random()]);
+
+        }
+
+        return false;
+    }
+
+    public function referralLink(): Attribute
+    {
+        return Attribute::make(function(){
+            return route('register', ['ref' => $this->referral_token]);
+        });
+    }
+
+
+}
