5fd13d6c4de29eab17432f9591828b3b6d826895
Mohamed Elogail <moh.elogail@gmail.com>
2023-10-05 15:03:12 +0300

    Fix Referral model namespace

--- src/app/Models/Referral.php
+++ src/app/Models/Referral.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Asciisd\ReferaralsLaravel\app\Models;
+namespace Asciisd\ReferralsLaravel\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
